d0e784c84996bb23e86fa2d6f368b5d2188380e4
Matheus Guilherme Schade <matheus_schade@hotmail.com>
2021-08-23 18:22:20 -0300

    exercicio-09

--- semana-3/revisao-js/exercicios.js
+++ semana-3/revisao-js/exercicios.js
@@ -92,7 +92,18 @@ function retornaNPrimeirosPares(n) {
 
 // EXERCÍCIO 09
 function classificaTriangulo(ladoA, ladoB, ladoC) {
-
+    
+   if (ladoA === ladoB && ladoB === ladoC) {
+      return "Equilátero"
+  } else if (ladoA === ladoB && ladoA !== ladoC) {
+      return "Isósceles"
+  } else if (ladoA === ladoC && ladoC !== ladoB) {
+      return "Isósceles"
+  } else if (ladoB === ladoC && ladoA !== ladoB) {
+      return "Isósceles"
+  } else {
+      return "Escaleno"
+  }
 }
 
 // EXERCÍCIO 10
